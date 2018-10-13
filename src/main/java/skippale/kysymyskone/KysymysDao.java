cb0e89e21ff6b4dd6f863ae31f652488d7195e15
Aleksi Skippari <aleksi.skippari@helsinki.fi>
2018-10-14 00:11:21 +0300

    nytko

--- src/main/java/skippale/kysymyskone/KysymysDao.java
+++ src/main/java/skippale/kysymyskone/KysymysDao.java
@@ -116,7 +116,7 @@ public class KysymysDao implements Dao<Kysymys, Integer>{
 
         while (tulos.next()) {
             String aihe = tulos.getString("aihe");
-            palautus.add(tulos.getString(aihe));
+            palautus.add(tulos.getString("aihe"));
             }
 
 
